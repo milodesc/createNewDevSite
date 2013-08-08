10bd1859d5349db9f751a6c71b431ea621e0efad
Patrick Jensen <patrick.c.jensen@gmail.com>
2013-08-07 22:37:15 -0400

    Added hosts file changes and added some comments re: planned development

--- createNewDevSite.sh
+++ createNewDevSite.sh
@@ -1,19 +1,31 @@
 #!/bin/sh
 
-#Prompt for URL
+#Check number of args
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 URL" >&2
+  exit 1
+fi
 
-#Add the URL to the /etc/hosts file
+#Append the URL/ip address pair to the /etc/hosts file
+sudo echo "127.0.0.1	$1" >> /etc/hosts
 
 #Check to make sure the URL is resolving locally
-#wget
+httpStatus=`curl -o /dev/null --silent --head --write-out '%{http_code}\n' $1`
+if [ "$httpStatus" -ne 200 ]; then
+  echo "Error setting up /etc/hosts file" >&2
+  exit 1
+fi
 
-#Copy the config file
-sudo cp /etc/apache2/sites-available/default /etc/apache2/sites-available/newSite2.com
+#Copy the config file and edit the copy
+#sudo cp /etc/apache2/sites-available/default /etc/apache2/sites-available/$1
+#ServerName $1
+#DocumentRoot /var/www/$1
+#<Directory /var/www/$1>
 
 #Create and populate the document root
-sudo mkdir -p /var/www/newSite2.com
-sudo echo "Congrats! You have a new dev site." > /var/www/newSite2.com/index.html
+#sudo mkdir -p /var/www/$1
+#sudo echo "Congrats! You have a new dev site." > /var/www/$1/index.html
 
 #Activate the site
-sudo a2ensite newSite2.com
-service apache2 reload
+#sudo a2ensite $1
+#service apache2 reload
