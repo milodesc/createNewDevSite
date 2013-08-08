2e1940d123a63c443d5bab0886314ff03e6385aa
Patrick Jensen <patrick.c.jensen@gmail.com>
2013-08-08 08:17:35 -0400

    Corrected the /etc/hosts error message.

--- createNewDevSite.sh
+++ createNewDevSite.sh
@@ -12,7 +12,7 @@ sudo echo "127.0.0.1	$1" >> /etc/hosts
 #Check to make sure the URL is resolving locally
 httpStatus=`curl -o /dev/null --silent --head --write-out '%{http_code}\n' $1`
 if [ "$httpStatus" -ne 200 ]; then
-  echo "Error setting up /etc/hosts file" >&2
+  echo "Error: Either your server isn't running or there was a problem changing the /etc/hosts file" >&2
   exit 1
 fi
 
