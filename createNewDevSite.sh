ea2e8aa1d4a932552645f83fa6df0b88eca86ffb
Patrick Jensen <patrick.c.jensen@gmail.com>
2013-08-08 08:21:18 -0400

    Assigned first parameter to a variable for readability

--- createNewDevSite.sh
+++ createNewDevSite.sh
@@ -6,26 +6,28 @@ if [ "$#" -ne 1 ]; then
   exit 1
 fi
 
+url=$1
+
 #Append the URL/ip address pair to the /etc/hosts file
-sudo echo "127.0.0.1	$1" >> /etc/hosts
+sudo echo "127.0.0.1	$url" >> /etc/hosts
 
 #Check to make sure the URL is resolving locally
-httpStatus=`curl -o /dev/null --silent --head --write-out '%{http_code}\n' $1`
+httpStatus=`curl -o /dev/null --silent --head --write-out '%{http_code}\n' $url`
 if [ "$httpStatus" -ne 200 ]; then
   echo "Error: Either your server isn't running or there was a problem changing the /etc/hosts file" >&2
   exit 1
 fi
 
 #Copy the config file and edit the copy
-#sudo cp /etc/apache2/sites-available/default /etc/apache2/sites-available/$1
-#ServerName $1
-#DocumentRoot /var/www/$1
-#<Directory /var/www/$1>
+#sudo cp /etc/apache2/sites-available/default /etc/apache2/sites-available/$url
+#ServerName $url
+#DocumentRoot /var/www/$url
+#<Directory /var/www/$url>
 
 #Create and populate the document root
-#sudo mkdir -p /var/www/$1
-#sudo echo "Congrats! You have a new dev site." > /var/www/$1/index.html
+#sudo mkdir -p /var/www/$url
+#sudo echo "Congrats! You have a new dev site." > /var/www/$url/index.html
 
 #Activate the site
-#sudo a2ensite $1
+#sudo a2ensite $url
 #service apache2 reload
